4f21c4caf37c9101146b4e8945dee6db03e18af7
heredialucas <heredialucasfac22@gmail.com>
2023-09-23 21:59:53 +1200

    chore: graphql init

--- index.js
+++ index.js
@@ -43,9 +43,7 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+const { url } = await startStandaloneServer(server);
 
 // eslint-disable-next-line no-console
 console.log(`🚀  Server ready at: ${url}`);
